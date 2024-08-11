9b4c772d5257a4e05e09319cecb374424919b6f2
mariotmc <mariotomicx@gmail.com>
2024-08-11 16:29:53 +0200

    s

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -5,5 +5,5 @@ class Lobby < ApplicationRecord
   enum status: { waiting: 0, in_game: 1, finished: 2 }
 
   before_create -> { self.code = SecureRandom.alphanumeric(6).downcase }
-  after_create_commit -> { self.game.create!}
+  after_create_commit -> { Game.create!(lobby: self) }
 end
