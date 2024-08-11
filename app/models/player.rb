46febd9f778e211f9615a6d00f2fc11f006c3a8f
mariotmc <mariotomicx@gmail.com>
2024-08-12 01:29:00 +0200

    add player join and leave broadcasts

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,14 @@
 class Player < ApplicationRecord
   belongs_to :lobby
+
+  after_create_commit -> { broadcast_player_created }
+  after_destroy_commit -> { broadcast_player_removed }
+
+  def broadcast_player_created
+    broadcast_append_to("lobby_#{lobby.id}_players", partial: "players/player", locals: { player: self }, target: "lobby_#{lobby.id}_players")
+  end
+
+  def broadcast_player_removed
+    broadcast_remove_to("lobby_#{lobby.id}_players", target: "player_#{self.id}")
+  end
 end
