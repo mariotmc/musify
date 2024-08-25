08c9ac699e231a2522ba098016bc65174d4f1342
mariotmc <mariotomicx@gmail.com>
2024-08-26 00:24:06 +0200

    action cable setup

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    def subscribed
+      stream_from current_player
+    end
+
+    def unsubscribed
+      current_player.unsubscribed
+    end
   end
 end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      self.current_player = find_verified_player
+    end
+
+    protected
+      def find_verified_player
+        if Current.player.present?
+          Current.player
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
