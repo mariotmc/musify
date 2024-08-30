e7b4dc4d2c3d6ed0aa07b3312f923b7f50698353
mariotmc <mariotomicx@gmail.com>
2024-08-30 18:16:06 +0200

    refactor job

--- app/jobs/timer_job.rb
+++ app/jobs/timer_job.rb
@@ -1,8 +1,10 @@
 class TimerJob < ApplicationJob
-  def perform(round:)
-    round = Round.find(round.id)
+  queue_as :default
 
+  def perform(round:, song:)
+    return unless round && song
     return if round.status == "scoreboard" || round.status == "ended"
+    return if song != round.current_song
 
     round.finish_current_song!
   end
