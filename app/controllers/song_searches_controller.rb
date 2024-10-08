614e037b964116f778b3231f4b8177af72731147
mariotmc <mariotomicx@gmail.com>
2024-10-08 23:17:50 +0200

    fix no song selected edge case

--- app/controllers/song_searches_controller.rb
+++ app/controllers/song_searches_controller.rb
@@ -2,9 +2,10 @@ class SongSearchesController < ApplicationController
   def index
     @player_id = params[:player_id]
     @round_id = params[:round_id]
+    @query = params[:query]
 
-    if params[:query].present?
-      @songs = RSpotify::Track.search(params[:query], limit: 6)
+    if @query.present?
+      @songs = RSpotify::Track.search(@query, limit: 6)
                               .select{|song| song.preview_url}
                               .map{|song| {id: song.id, title: song.name, artist: song.artists.first.name, image: song.album.images.second["url"], preview_url: song.preview_url}}
     else
@@ -13,6 +14,8 @@ class SongSearchesController < ApplicationController
   end
 
   def create
+    redirect_to song_searches_path(query: params[:query]) and return if params[:spotify_id].blank?
+
     @round = Round.find(params[:round_id])
     @player = Player.find(params[:player_id])
     @song = RSpotify::Track.find(params[:spotify_id])
