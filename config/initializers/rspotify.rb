005b707bfa441ee6d1717feb03355e98e025f978
mariotmc <mariotomicx@gmail.com>
2024-11-18 18:27:35 +0100

    try auth bypass

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,3 +1,5 @@
-Rails.application.config.after_initialize do
-  RSpotify.authenticate(Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret)
+unless Rails.env.test? || ENV["SECRET_KEY_BASE_DUMMY"]
+  Rails.application.config.after_initialize do
+    RSpotify.authenticate(Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret)
+  end
 end
