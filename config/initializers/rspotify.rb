f8fec714b1581748e35554526545385b0b5f849f
mariotmc <mariotomicx@gmail.com>
2024-11-24 14:09:48 +0100

    refactor spotify auth

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,15 +1,8 @@
-if Rails.env.production?
-  begin
-    Rails.application.config.after_initialize do
-      if Rails.application.credentials.spotify_client_id.present? &&
-         Rails.application.credentials.spotify_client_secret.present?
-        RSpotify.authenticate(
-          Rails.application.credentials.spotify_client_id,
-          Rails.application.credentials.spotify_client_secret
-        )
-      end
-    end
-  rescue StandardError => e
-    Rails.logger.error "Failed to initialize RSpotify: #{e.message}"
-  end
+begin
+  RSpotify.authenticate(
+    Rails.application.credentials.spotify_client_id,
+    Rails.application.credentials.spotify_client_secret
+  )
+rescue StandardError => e
+  Rails.logger.error "Failed to initialize RSpotify: #{e.message}"
 end
